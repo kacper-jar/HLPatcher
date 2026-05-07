30c7a74e299fcfd9c0e490f834a7d8b2386b7b96
Kacper Jarosławski <kacper.jaroslawski@kzl21.ovh>
2026-05-07 14:31:22 +0200

    fix: update warning page to include portal

--- patcher/ui/pages/warning_page.py
+++ patcher/ui/pages/warning_page.py
@@ -11,19 +11,20 @@ class WarningPage(BasePage):
 
         warning_text = (
             "Before proceeding, you MUST switch your game\n"
-            "installations to the 'steam_legacy' branch.\n\n"
+            "installations to the correct legacy branches.\n\n"
             "Set these games in this order (to avoid resets),\n"
             "skipping any you are not patching:\n"
             "1. Half-Life: Source\n"
             "2. Half-Life 2: Episode One\n"
             "3. Half-Life 2: Episode Two\n"
             "4. Half-Life 2: Lost Coast\n"
-            "5. Half-Life 2\n\n"
+            "5. Half-Life 2\n"
+            "6. Portal\n\n"
             "To do this:\n"
             "1. Open Steam\n"
             "2. Right-click Game -> Properties\n"
             "3. Go to Betas\n"
-            "4. Select 'steam_legacy - Pre-20th Anniversary Build'\n\n"
+            "4. Select 'steam_legacy' (or 'beta' for Portal)\n\n"
             "Failure to do so will result in a broken game.\n\n"
             "Click Next once the branches are changed and Steam\n"
             "finishes downgrading the files."
@@ -39,7 +40,7 @@ class WarningPage(BasePage):
         warning_label.pack(fill="x", padx=15, pady=15)
 
     def get_title(self) -> str:
-        return "Before you continue!"
+        return "Before You Continue!"
 
     def get_next_page_key(self) -> str:
         return "progress"
